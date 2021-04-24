36a1cc3306082e386589f789bc463c5b11a34e54
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-04-25 00:23:53 +0100

    Added Front page part 1. Avatar and GridViewIcon added.

--- google-2-yt/pages/index.js
+++ google-2-yt/pages/index.js
@@ -1,13 +1,39 @@
 import Head from 'next/head'
 
+import Avatar from "../components/Avatar"
+import { ViewGridIcon } from "@heroicons/react/solid"
+
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Google</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Lets Build Google with Next.JS and tailwind</h1>
+      {/* Header */}
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        {/* Left */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
+        </div>
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+
+          {/* Icon */}
+          <ViewGridIcon className="h-10 w-10 p" />
+
+          {/* Avatar */}
+          <Avatar url="https://unsplash.com/photos/OhKElOkQ3RE" />
+          {/* <Avatar url={"https://coaching.papareact.com/a19"} /> */}
+        </div>
+      </header>
+
+      {/* Body dw */}
+
+      {/* Footer */}
     </div>
   )
 }
