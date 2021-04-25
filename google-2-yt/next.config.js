42643b884c0a6e1938cd6e366e3dca05a01a0852
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-04-25 01:29:22 +0100

    Added Front page part 2. Includes Google logo. Use next/image Image component to render image and configure next.config.js file. Added Form and input buttons.

---
+++ google-2-yt/next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    images: {
+        domains: ["images.unsplash.com", "unsplash.com",
+            "www.google.com", "www.google.co.uk"],
+    },
+};
--- google-2-yt/pages/index.js
+++ google-2-yt/pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 
 import Avatar from "../components/Avatar"
-import { ViewGridIcon } from "@heroicons/react/solid"
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid"
+import { SearchIcon } from "@heroicons/react/outline"
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -23,7 +25,7 @@ export default function Home() {
           <p className="link">Images</p>
 
           {/* Icon */}
-          <ViewGridIcon className="h-10 w-10 p" />
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
 
           {/* Avatar */}
           <Avatar url="https://unsplash.com/photos/OhKElOkQ3RE" />
@@ -32,6 +34,25 @@ export default function Home() {
       </header>
 
       {/* Body dw */}
+      <form className="flex flex-col items-center mt-44 flex-grow">
+        <Image
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={100}
+          width={300}
+        />
+
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl" >
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn" >Google Search</button>
+          <button className="btn" >I'm Feeling Lucky</button>
+        </div>
+
+      </form>
 
       {/* Footer */}
     </div>
