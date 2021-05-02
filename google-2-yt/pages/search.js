1eb806dc8cd466e92c5e81d6dd6a50d29c640f48
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-05-03 00:13:33 +0100

    Amended the env variables to be used in Vercel.

--- google-2-yt/pages/search.js
+++ google-2-yt/pages/search.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router"
 
 import Header from "../components/Header"
 import SearchResults from "../components/SearchResults"
-import { API_KEY, CONTEXT_KEY } from "../keys"
+// import { API_KEY, CONTEXT_KEY } from "../keys"
 import Response from "../Response"
 
 function Search({ results }) {
@@ -31,7 +31,7 @@ export async function getServerSideProps(context) {
     const startIndex = context.query.start || "0"
 
     const data = useDummyData ? Response : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
     ).then(response => response.json());
 
     // After the SERVER has rendered... Pass the results to the client...
